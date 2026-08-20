2cb5b17999168e448013459f4a9e7695da317c4b
27samara1c <samara.muniz.silva@escola.pr.gov.br>
2026-08-20 19:33:28 -0300

    p

--- script.js
+++ script.js
@@ -1,56 +1,32 @@
-
 const botoes = document.querySelectorAll("button");
 
-botoes.forEach(function (botao) {
+botoes.forEach(function(botao){
     let curtiu = false;
-
-    botao.addEventListener("click", function () {
+    botao.addEventListener("click",botaoClicado);
+    function botaoClicado(){
+        console.log("fui clicado");
         let texto = botao.querySelector("span");
-        
-    
-        if (!texto) return;
-
-        let total = parseInt(texto.textContent, 10) || 0;
-
-        if (curtiu === false) {
-            texto.textContent = total + 1;
+        if(curtiu === false){
+            texto.textContent++;
             curtiu = true;
-        } else {
-            texto.textContent = total - 1;
+        } else{
+            texto.textContent--;
             curtiu = false;
-        }
-    });
-});
+        }  
+    }
 
+})
 
 const btnTemaEscuro = document.querySelector(".bnt-tema-escuro");
 
-if (btnTemaEscuro) {
-    btnTemaEscuro.addEventListener("click", function () {
-        document.body.classList.toggle("tema-escuro");
-    });
+btnTemaEscuro.addEventListener("click", mudaTema);
+
+function mudaTema() {
+    const corpoPagina = document.body;
+    if (corpoPagina.classList.contains("tema-escuro")) {
+        corpoPagina.classList.remove("tema-escuro");
+    } else {
+        corpoPagina.classList.add("tema-escuro");
+    }
 }
-// Seleciona todos os botões da página
-const botoes = document.querySelectorAll("button");
-
-botoes.forEach(function (botao) {
-    let curtiu = false;
-
-    botao.addEventListener("click", function () {
-        let texto = botao.querySelector("span");
-        
-        // Se o botão não tiver um <span> dentro (como o botão de tema), ele ignora a contagem
-        if (!texto) return;
-
-        let total = parseInt(texto.textContent, 10) || 0;
-
-        if (curtiu === false) {
-            texto.textContent = total + 1;
-            curtiu = true;
-        } else {
-            texto.textContent = total - 1;
-            curtiu = false;
-        }
-    });
-});
 
